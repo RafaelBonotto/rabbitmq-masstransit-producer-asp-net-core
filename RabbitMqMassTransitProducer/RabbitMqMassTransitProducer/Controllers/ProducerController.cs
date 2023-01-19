8bb3f807fef837437b097d02a145e4c4da83288f
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-18 21:55:51 -0300

    Criação da Controller

---
+++ RabbitMqMassTransitProducer/RabbitMqMassTransitProducer/Controllers/ProducerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMqMassTransitProducer.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        [HttpPost("producer")]
+        public async Task<IActionResult> InsertMessage()
+        {
+            return Ok();
+        }
+    }
+}

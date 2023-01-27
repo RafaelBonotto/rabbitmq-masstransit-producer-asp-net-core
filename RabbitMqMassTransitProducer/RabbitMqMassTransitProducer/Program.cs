2458ea90669bad86dc4842d003c51319aca066c8
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-26 23:30:58 -0300

    Update Program.cs

--- RabbitMqMassTransitProducer/RabbitMqMassTransitProducer/Program.cs
+++ RabbitMqMassTransitProducer/RabbitMqMassTransitProducer/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq();
+});
 
 var app = builder.Build();
 
